fd1c0aa1fe6e98cd8e13ae42f0c48b0abf6106a6
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-10-20 22:49:11 +0300

    ДЗ Cars

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Car {
     private final String brand;
     private final String model;
@@ -31,14 +33,27 @@ public class Car {
     }
 
 
-
     public Car(String brand, String model, int productionYear, String productionCountry, String color,
                double engineVolume) {
-        this.brand = brand;
-        this.model = model;
-        this.productionCountry = productionCountry;
-        this.color = color;
-        this.engineVolume = engineVolume;
-        this.productionYear = productionYear;
+        String def = "default";
+        this.brand = Objects.requireNonNullElse(brand, def);
+        this.model = Objects.requireNonNullElse(model, def);
+        this.productionCountry = Objects.requireNonNullElse(productionCountry, def);
+
+        String defColor = "белый";
+        this.color = Objects.requireNonNullElse(color, defColor);
+
+        double defEngineVolume = 1.5;
+        if (engineVolume <= 0) {
+            this.engineVolume = defEngineVolume;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+
+        int defProductionYear = 2000;
+        if (productionYear <= 0) {
+            this.productionYear = defProductionYear;
+        } else
+            this.productionYear = productionYear;
     }
 }
